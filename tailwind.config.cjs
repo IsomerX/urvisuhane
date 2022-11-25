fe218e8775aa8604500b4a51ed496a215c76aec2
IsomerX <49413460+IsomerX@users.noreply.github.com>
2022-11-26 04:28:14 +0530

    banner

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                voyage: ["Voyage", "sans-serif"],
+                mont: ["Montserrat", "sans-serif"],
+            },
+            colors: {
+                pink: "#9F67C1",
+                "pink-light": "#D9B8E6",
+            },
+        },
+    },
+    plugins: [],
+};
