bc178070ba7d68065c3a2ffd52a5465b4bfe090f
IsomerX <49413460+IsomerX@users.noreply.github.com>
2022-11-26 21:09:20 +0530

    added animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
             },
             colors: {
                 pink: "#9F67C1",
-                "pink-light": "#D9B8E6",
+                "pink-light": "#543A6D",
                 gray: "#909090",
             },
         },
